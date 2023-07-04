af9994d855f89a4543d752f9c310e42f65fc8c64
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-04 16:53:55 +0200

    Esercizio completo e aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-// CICLO FOR CON CONDIZIONI
-for (var i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
+// CICLO FOR CON CONDIZIONI CONSOLE
+for (let i = 1; i < 101; i++) {
+    if (i % 15 == 0) console.log("fizzbuzz");
+    else if (i % 3 == 0) console.log("fizz");
+    else if (i % 5 == 0) console.log("buzz");
     else console.log(i);
+}
+
+// DICHIARAZIONE VARIABILI
+const chessContainer = document.querySelector(".chess-container");
+const totalCells = 10*10;
+
+// CICLO FOR PER SCACCHIERA
+for (let i = 1; i < totalCells; i++){
+    let singleCell = `<div class="chess-square"></div>`;
+
+    chessContainer.innerHTML + singleCell;
+
+    singleCell = document.createElement ("div");
+    singleCell.classList.add("chess-square");
+    singleCell.classList.add("d-flex", "justify-content-center", "align-items-center", "bg-primary");
+    singleCell.innerHTML = i.toString();
+
+    // CONDIZIONI
+    if (i % 15 == 0) {
+        singleCell.classList.add("bg-danger")
+        singleCell.innerHTML = ("fizzbuzz")
+    } else if (i % 3 == 0) {
+        singleCell.classList.add("bg-warning")
+        singleCell.innerHTML = ("fizz")
+    } else if (i % 5 == 0) {
+        singleCell.classList.add("bg-success")
+        singleCell.innerHTML = ("buzz")
+    }    
+    chessContainer.append(singleCell);
+
 }
